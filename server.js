42fe3104ac16ec62f82777f3f81d8133089d16ad
David Park <dysp92@gmail.com>
2018-01-15 09:29:41 -0800

    server starting fixed

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ function closeServer() {
 }
   
 
-
-
-// app.listen(process.env.PORT || 8080);
-
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+  
 module.exports = { runServer, app, closeServer };
