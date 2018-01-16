6bd7c70fc2c7a53f1fcf68fc771707c5c9f771ae
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-01-15 22:07:54 -0800

    started front-end a very little

---
+++ public/index.js
@@ -0,0 +1,35 @@
+'use strict';
+/* use global $ */
+
+
+const STORE = {
+  view: 'home', //home, search, add, search-results, read
+
+};
+
+
+
+function renderPage(){
+  switch(STORE.view){
+  case 'home':
+    break;
+  case 'search':
+    break;
+  case 'add':
+    break;
+  case 'search-results':
+    break;
+  case 'read':
+  }
+}
+
+
+
+
+
+
+
+
+$(function(){
+  renderPage();
+});
