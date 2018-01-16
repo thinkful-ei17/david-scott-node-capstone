cae111c2efdf0cac2d334a99a090af2a39ea8a1a
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-01-16 11:43:17 -0800

    thought it was master, merging now

--- public/app.js
+++ public/app.js
@@ -1,7 +1,27 @@
 /* global $ */
 'use strict';
 
+const STORE = {
+  view: 'home', //home, search, search-results, add, read
+};
 
+function generateHomePageHTML(){
+  return `
+  <header>   
+    <h1>Welcome to Song Cellar!</h1>
+    <h2>Refresh your memory while filing your glass!</h2>
+  </header>
+
+  <form>
+    <button class='to-search, js-to-search' id='to-search'>To the Search!</button> 
+    <button class='to-add, js-add' id='to-add'>Add Lyrics Here!</button>
+  </form>
+  `
+}
+
+function renderHomePage(){
+  $('main').html(generateHomePageHTML());
+}
 
 function generateAddPageHTML() {
   return `
@@ -46,6 +66,61 @@ function renderReadPage() {
   return generateReadPageHTML;
 }
 
+function renderSearchForm(){
+  console.log('renderSearchForm ran');
+  $('main').html(generateSearchFormHTML());
+}
+
+function generateSearchFormHTML(){
+    return `
+      <form name='search-form' class='search-form, js-search-form' id='search-form'>
+        <h2 for='search-form'>
+        Search the Database!
+      </h2>
+      <lable for='title-input' class='search-lable'>Search by Title</lable>
+      <input type='text' class='title-input, js-title-input' id='title-input' form='search-form' placeholder='title to search'>
+      <lable for='title-search' class='search-lable'>Search By User</lable>
+      <select name='title-search' class='title-search, js-title-search' id='title-search' form='search-form'>
+        <option>song 1</option>
+        <option>song 2</option>
+        <option>song 3</option>
+      </select>
+      <button class='search-button, js-search-button'>
+        Search Now!
+      </button>
+      </form>
+    `
+}
+
+function generateResultsHTML(){
+  console.log('generateSearchResultsHTML ran');
+
+  return `
+    <header>
+      <h1>Pick A Song!</h1>
+    </header>
+
+    <section class='show-results, js-show-results'>
+      <ul class='results-list, js-results-list'>
+        <li class='search-result'><a href=''>Song 1</a></li>
+        <li class='search-result'><a href=''>Song 2</a></li>
+        <li class='search-result'><a href=''>Song 3</a></li>
+      </ul>
+    </section>
+  `
+}
+
+function renderSearchResults(){
+  console.log('renderSearchResults ran');
+ $('main').html(generateResultsHTML());
+}
+
+
+// renderSearchForm();
+renderSearchResults();
+
+
+
 
 
 
@@ -73,6 +148,8 @@ function renderPage() {
 
 
 $(() => {
+  renderPage();
+
   $('.nav-bar').on('click', '#nav-home', () => {
     STORE.view = 'home';
     renderPage();
